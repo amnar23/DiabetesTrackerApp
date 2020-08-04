1e0201bb5ea4db44862f4f4e66107a8a92c94116
Amna <amnar23498@gmail.com>
2020-08-04 16:51:34 +0500

    Reminder Activity adjusted

--- app/src/main/java/com/example/diabetestracker/activities/Reminder.java
+++ app/src/main/java/com/example/diabetestracker/activities/Reminder.java
@@ -32,9 +32,6 @@ public class Reminder extends AppCompatActivity implements AdapterView.OnItemSel
     TimePicker tp;
     Spinner spinner;
     ArrayAdapter<CharSequence> adapter;
-    TimePickerDialog timepicker;
-    DatePickerDialog datepicker;
-    EditText edate,etime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,10 +43,10 @@ public class Reminder extends AppCompatActivity implements AdapterView.OnItemSel
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
-       // tp=findViewById(R.id.time);
-       // tp.setIs24HourView(false);
-        edate=findViewById(R.id.date);
-        etime=findViewById(R.id.time);
+
+        tp=findViewById(R.id.time);
+        tp.setIs24HourView(false);
+
         //back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -68,82 +65,19 @@ public class Reminder extends AppCompatActivity implements AdapterView.OnItemSel
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) { }
-    //Date Picker
-    public void pickDate(View v){
-        final Calendar cldr = Calendar.getInstance();
-        int day = cldr.get(Calendar.DAY_OF_MONTH);
-        int month = cldr.get(Calendar.MONTH);
-        int year = cldr.get(Calendar.YEAR);
-        // date picker dialog
-        datepicker = new DatePickerDialog(Reminder.this,
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        String month,day;
-                        month=String.valueOf(monthOfYear+1);
-                        day=String.valueOf(dayOfMonth);
-                        if(monthOfYear < 10){
-                            month = "0" + month;
-                        }
-                        if(dayOfMonth < 10){
-                            day  = "0" + day ;
-                        }
-                        edate.setText(year + "/" + month + "/" + day);
-                    }
-                }, year, month, day);
-        datepicker.show();
-    }
-    //Time Picker
-    public void pickTime(View v){
-        final Calendar cldr = Calendar.getInstance();
-        int hour = cldr.get(Calendar.HOUR_OF_DAY);
-        int minutes = cldr.get(Calendar.MINUTE);
-        String am_pm=" ";
-        if(cldr.get(Calendar.AM_PM)==Calendar.AM)
-            am_pm="am";
-        else if(cldr.get(Calendar.AM_PM)==Calendar.PM)
-            am_pm="pm";
-        // time picker dialog
-        timepicker = new TimePickerDialog(Reminder.this,
-                new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
-                        String time=sHour+":"+sMinute;
-                        SimpleDateFormat fmt=new SimpleDateFormat("hh:mm");
-                        Date date=null;
-                        try{
-                            date=fmt.parse(time);
-                        } catch (ParseException e) {
-                            e.printStackTrace();
-                        }
-                        SimpleDateFormat fmtOut = new SimpleDateFormat("hh:mm aa");
 
-                        String formattedTime=fmtOut.format(date);
-                        etime.setText(formattedTime);
-                    }
-                }, hour, minutes, false);
-        timepicker.show();
-    }
+
     public void setReminder(View v) {
         int hour, minute;
-       // hour = tp.getCurrentHour();
-        //minute = tp.getCurrentMinute();
-        hour=Integer.parseInt(String.valueOf(etime.getText()).substring(0,2));
-        minute=Integer.parseInt(String.valueOf(etime.getText()).substring(3,5));
-        String am_pm=String.valueOf(etime.getText()).substring(6);
-        boolean t;
-        if(am_pm=="AM")
-            t=false;
-        else
-            t=true;
+        hour = tp.getCurrentHour();
+        minute = tp.getCurrentMinute();
+
         Snackbar.make(v, String.valueOf(hour)+":"+String.valueOf(minute), Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
         Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
-                .putExtra(AlarmClock.EXTRA_DAYS,edate.getText())
                 .putExtra(AlarmClock.EXTRA_MESSAGE, field)
                 .putExtra(AlarmClock.EXTRA_HOUR, hour)
-                .putExtra(AlarmClock.EXTRA_MINUTES, minute)
-                .putExtra(AlarmClock.EXTRA_IS_PM,t);
+                .putExtra(AlarmClock.EXTRA_MINUTES, minute);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
